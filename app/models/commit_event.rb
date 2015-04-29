f517e3e1f96a886b7fab4e1910137a61aea3a7be
g. nicholas d'andrea <gnidan@dramafever.com>
2015-04-29 14:41:59 -0400

    Making events start when they happened and ending +1 hour

--- app/models/commit_event.rb
+++ app/models/commit_event.rb
@@ -8,8 +8,8 @@ class CommitEvent
 
   def to_ical
     event = Icalendar::Event.new
-    event.start = (@date - 1.hour).strftime("%Y%m%dT%H%M%S")
-    event.end = @date.strftime("%Y%m%dT%H%M%S")
+    event.start = @date.strftime("%Y%m%dT%H%M%S")
+    event.end = (@date + 1.hour).strftime("%Y%m%dT%H%M%S")
     event.summary = @source
     event.description ="#{@sha}\n\n#{@message}"
     event.location = "computer"
